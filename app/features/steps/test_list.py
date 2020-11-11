a7372df5f8ea9903b118b089c1e25cf85d564358
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-11-11 09:11:19 -0300

    fixing travis

--- app/features/steps/test_list.py
+++ app/features/steps/test_list.py
@@ -1,13 +1,16 @@
 from behave import given, when, then
 import requests
 
-api_url=None
+api_url = None
+bff_url = None
+
 
 @given('a pagina de gerenciar usuarios')
 def step_impl_given(context):
     global api_url
     api_url = 'https://smartvit-user-dev.herokuapp.com/user'
     print('url :'+api_url)
+
 
 @when('ele visualizar os usuarios desejados')
 def step_impl_when(context):
@@ -17,5 +20,7 @@ def step_impl_when(context):
 
 @then('o bff requisita o microsservico desejado')
 def step_impl_then(context):
-    response = requests.get('https://smartvit-admin-bff-dev.herokuapp.com/user/')
+    global bff_url 
+    bff_url = 'https://smartvit-admin-bff-dev.herokuapp.com/user/'
+    response = requests.get(bff_url)
     assert response.status_code == 200
