832c7f22fc331573d6e7697501494c035fcfdbc8
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-11-10 23:14:26 -0300

    fixing errors

--- app/features/steps/test_create.py
+++ app/features/steps/test_create.py
@@ -15,24 +15,24 @@ def step_impl_given(context):
 
 @when('ele regista novo conteúdo da solicitação')
 def step_impl_when(context):
-    request_bodies['POST'] = {"name": "Joao Ninguem", 
+    request_bodies['POST'] = {"name": "Joao Ninguem",
                               "cpf": "3654128900",
                               "email": "teste@gmail.com",
                               "password": "30061998",
                               "type": "Agricultor",
-                              "situation": "Ativo"
-                             }
+                              "situation": "Ativo"}
     response = requests.post(
                             'https://smartvit-user-dev.herokuapp.com/user',
-                             json=request_bodies['POST']
+                            json=request_bodies['POST']
                             )
     assert response.status_code == 200
 
 
 @then('o bff requisita o microsservico para criar informacao')
 def step_impl_then(context):
+    api_bff_url = 'https://smartvit-admin-bff-dev.herokuapp.com/user/'
     response = requests.post(
-                            'https://smartvit-admin-bff-dev.herokuapp.com/user/',
-                             json=request_bodies['POST']
+                            api_bff_url,
+                            json=request_bodies['POST']
                             )
     assert response.status_code == 200
